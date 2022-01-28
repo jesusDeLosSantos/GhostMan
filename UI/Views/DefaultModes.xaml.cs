403e7e26629f66cb5fe22e0b2c576bd7d1a0b8ea
sisoy <albert.machio@iesnervion.es>
2022-01-28 12:26:15 +0100

    vista de defaultmodes hecha, faltan imagenes, posible cambio de estilo + solucion de conflictos

---
+++ UI/Views/DefaultModes.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UI.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UI.Views
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class VistaJugar : Page
+    {
+        DefaultModesVM myViewModel;
+        public VistaJugar()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myViewModel = this.DataContext as DefaultModesVM;
+            myViewModel.IsEasyButtonPressed = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            myViewModel = this.DataContext as DefaultModesVM;
+            myViewModel.IsMediumButtonPressed = true;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            myViewModel = this.DataContext as DefaultModesVM;
+            myViewModel.IsHardButtonPressed = true;
+        }
+    }
+}
