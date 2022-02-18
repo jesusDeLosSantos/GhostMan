f72e9c3d8f269680df1dc9674119d1c7af311ec6
Alex <alexruiz29del12@gmail.com>
2022-02-18 13:21:06 +0100

    PlayVM movilidad enemigos

---
+++ UI/Models/Enemigo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.ViewModels.Utilities;
+
+namespace UI.Models
+{
+    public class Enemigo : clsVMBase
+    {
+
+        public Enemigo() {
+            X = 0;
+            Y = 0;
+        }
+
+        public Enemigo(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static int JugadorX { get; set; }
+        public static int JugadorY { get; set; }
+
+        public static bool usuarioVivo = true;
+
+        public async void mover()
+        {
+            Random random = new Random();
+            int aletorio;
+            while (usuarioVivo)
+            {
+                aletorio = random.Next(4);
+                switch (aletorio)
+                {
+                    case 0: //IZQ
+                        while (X - 50 >= 0 && usuarioVivo)
+                        {
+                            X -= 50;  NotifyPropertyChanged("X");
+                          
+                            comprobarEliminarUsuario();
+                            await Task.Delay(200);
+                        }
+                        break;
+                    case 1: //DER
+                        while (X + 50 < 1500 && usuarioVivo)
+                        {
+                            X += 50;NotifyPropertyChanged("X");
+                            
+                            comprobarEliminarUsuario();
+                            await Task.Delay(200);
+                        }
+                        break;
+                    case 2: //ARR
+                        while (Y - 50 >= 0 && usuarioVivo)
+                        {
+                            Y -= 50;
+                            comprobarEliminarUsuario();
+                            NotifyPropertyChanged("Y");
+                            await Task.Delay(200);
+                        }
+                        break;
+
+                    case 3: //ABA
+                        while (Y + 50 < 800 && usuarioVivo)
+                        {
+                            Y += 50; 
+                            comprobarEliminarUsuario();
+                            NotifyPropertyChanged("Y");
+                            await Task.Delay(200);
+                        }
+                        break;
+                }
+            }
+        }
+        private void comprobarEliminarUsuario()
+        {
+            if (X == JugadorX && Y == JugadorY)
+            {
+                usuarioVivo = false;
+            }
+        }
+    }
+}
--- UI/ViewModels/PlayVM.cs
+++ UI/ViewModels/PlayVM.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using UI.ViewModels.Utilities;
 using Windows.System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;
+using UI.Models;
 
 namespace UI.ViewModels
 {
@@ -16,89 +18,127 @@ namespace UI.ViewModels
         private bool teclaPulsadaAB = false;
         private bool teclaPulsadaL = false;
 
+        public Visibility VisibilidadUsuario { get; set; }
+
         public int X { get; set; }
         public int Y { get; set; }
 
+        public int EnemigoX { get; set; }
+        public int EnemigoY { get; set; }
+
+        public int EnemigoX2 { get; set; }
+        public int EnemigoY2 { get; set; }
+
+        public int EnemigoX3 { get; set; }
+        public int EnemigoY3 { get; set; }
+
+        public Enemigo Enemigo1 { get; set; }
+        public Enemigo Enemigo2 { get; set; }
+        public Enemigo Enemigo3 { get; set; }
+
+        public PlayVM()
+        {
+            X = 500;
+            Y = 500;
+         
+            Enemigo1 = new Enemigo(1000,300);
+            Enemigo1.mover();
+
+            Enemigo2 = new Enemigo();
+            Enemigo2.mover();
+
+            Enemigo3 = new Enemigo(200,700);
+            Enemigo3.mover();
+
+        }
+
         public async void moverFantasma(object sender, KeyRoutedEventArgs e)
         {
-            bool pared = false;
-            if (e.Key == VirtualKey.Right && !teclaPulsadaR)
+            if (Enemigo.usuarioVivo)
             {
-                teclaPulsadaR = true;
-                teclaPulsadaAR = false;
-                teclaPulsadaAB = false;
-                teclaPulsadaL = false;
-                while (!pared && teclaPulsadaR)
+                bool pared = false;
+                if (e.Key == VirtualKey.Right && !teclaPulsadaR)
                 {
-                    if (X + 50 < 1500)
+                    teclaPulsadaR = true;
+                    teclaPulsadaAR = false;
+                    teclaPulsadaAB = false;
+                    teclaPulsadaL = false;
+                    while (!pared && teclaPulsadaR && Enemigo.usuarioVivo)
                     {
-                        X += 50;
-                        NotifyPropertyChanged("X");
-                        await Task.Delay(200); //System.Threading.Tasks.Task.Delay(200);
-                    }
-                    else
-                    {
-                        pared = true;
+                        if (X + 50 < 1500)
+                        {
+                            X += 50;
+                            NotifyPropertyChanged("X");
+                            Enemigo.JugadorX = X;
+                            await Task.Delay(200); //System.Threading.Tasks.Task.Delay(200);
+                        }
+                        else
+                        {
+                            pared = true;
+                        }
                     }
                 }
-            }
-            else if (e.Key == VirtualKey.Left && !teclaPulsadaL)
-            {
-                teclaPulsadaR = false;
-                teclaPulsadaAR = false;
-                teclaPulsadaAB = false;
-                teclaPulsadaL = true;
-                while (!pared && teclaPulsadaL)
+                else if (e.Key == VirtualKey.Left && !teclaPulsadaL)
                 {
-                    if (X - 50 >= 0)
+                    teclaPulsadaR = false;
+                    teclaPulsadaAR = false;
+                    teclaPulsadaAB = false;
+                    teclaPulsadaL = true;
+                    while (!pared && teclaPulsadaL && Enemigo.usuarioVivo)
                     {
-                        X -= 50;
-                        NotifyPropertyChanged("X");
-                        await Task.Delay(200);
-                    }
-                    else
-                    {
-                        pared = true;
+                        if (X - 50 >= 0)
+                        {
+                            X -= 50;
+                            NotifyPropertyChanged("X");
+                            Enemigo.JugadorX = X;
+                            await Task.Delay(200);
+                        }
+                        else
+                        {
+                            pared = true;
+                        }
                     }
                 }
-            }
-            else if (e.Key == VirtualKey.Up && !teclaPulsadaAR)
-            {
-                teclaPulsadaR = false;
-                teclaPulsadaAR = true;
-                teclaPulsadaAB = false;
-                teclaPulsadaL = false;
-                while (!pared && teclaPulsadaAR)
+                else if (e.Key == VirtualKey.Up && !teclaPulsadaAR)
                 {
-                    if (Y - 50 >= 0)
+                    teclaPulsadaR = false;
+                    teclaPulsadaAR = true;
+                    teclaPulsadaAB = false;
+                    teclaPulsadaL = false;
+                    while (!pared && teclaPulsadaAR && Enemigo.usuarioVivo)
                     {
-                        Y -= 50;
-                        NotifyPropertyChanged("Y");
-                        await Task.Delay(200);
-                    }
-                    else
-                    {
-                        pared = true;
+                        if (Y - 50 >= 0)
+                        {
+                            Y -= 50;
+                            NotifyPropertyChanged("Y");
+                            Enemigo.JugadorY = Y;
+                            await Task.Delay(200);
+                        }
+                        else
+                        {
+                            pared = true;
+                        }
                     }
                 }
-            }
-            else if (e.Key == VirtualKey.Down && !teclaPulsadaAB)
-            {
-                teclaPulsadaR = false;
-                teclaPulsadaAR = false;
-                teclaPulsadaAB = true;
-                teclaPulsadaL = false;
-                while (!pared && teclaPulsadaAB)
+                else if (e.Key == VirtualKey.Down && !teclaPulsadaAB)
                 {
-                    if (Y + 50 < 800)
+                    teclaPulsadaR = false;
+                    teclaPulsadaAR = false;
+                    teclaPulsadaAB = true;
+                    teclaPulsadaL = false;
+                    while (!pared && teclaPulsadaAB && Enemigo.usuarioVivo)
                     {
-                        Y += 50;
-                        NotifyPropertyChanged("Y");
-                        await Task.Delay(200);
-                    }
-                    else
-                    {
-                        pared = true;
+                        if (Y + 50 < 800)
+                        {
+                            Y += 50;
+                            NotifyPropertyChanged("Y");
+                            Enemigo.JugadorY = Y;
+                            await Task.Delay(200);
+                        }
+                        else
+                        {
+                            pared = true;
+                        }
                     }
                 }
             }
