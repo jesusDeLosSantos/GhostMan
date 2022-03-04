1af264fc0c8fd37352a038ae4681c0e59020dec7
Kanadista <sebastian.forte@iesnervion.es>
2022-03-04 10:57:13 +0100

    Views style added

--- UI/Views/App.xaml.cs
+++ UI/Views/App.xaml.cs
@@ -68,7 +68,7 @@ namespace UI
                     // configurando la nueva página pasándole la información requerida como
                     //parámetro de navegación
                     rootFrame.Navigate(typeof(Views.MapCreator), e.Arguments);
-                    rootFrame.Navigate(typeof(Play), e.Arguments);
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Asegurarse de que la ventana actual está activa.
                 Window.Current.Activate();
